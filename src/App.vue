d51ee6c88694c2e3c1cb216451671265fc2ac874
Nicolas BUSSER <nicolas.busser67@gmail.com>
2022-01-27 13:19:35 +0100

    add basic component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>Lampemetre</h1>
+  <Workspace msg="Ceci est un prop"/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Workspace from './components/Workspace.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
+    Workspace,
   },
 });
 </script>
